668d1d545a33b2481cbefc9d26532e262f458f14
grandballoon <luke.sovern.conley@gmail.com>
2018-08-03 10:23:16 -0400

    added reviews

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -6,12 +6,18 @@ class ActorsController < ApplicationController
   end
 
   def update
+
     @actor = Actor.find(params[:id])
     if @actor.update(actor_params)
       redirect_to #TODO
     else
       render #TODO
     end
+  end
+
+  def show
+    @actor = Actor.find(params[:id])
+    @reviews = ["#{@actor.name} is spellbinding in #{@actor.shows.sample.title}", "#{@actor.name} gives the performance of a lifetime in #{@actor.shows.sample.title}", "#{@actor.name} is back with a vengeance", "#{@actor.shows.sample.title} is the best #{@actor.name}'s been in years", "Mark my words: #{@actor.shows.sample.title} is the beginning of the #{@actor.name}aissance", "#{@actor.name} is sure to win a Daytime Emmy for #{@actor.shows.sample.title}", "#{@actor.name} sneaks up on you and floors you in #{@actor.shows.sample.title}", "#{@actor.name}. Is. Back.", "If you're not watching #{@actor.name} in #{@actor.shows.sample.title}, what are you even doing?", "One word for #{@actor.name}'s most recent performance: Superb", "If there's a stronger shoe-in for an Emmy than #{@actor.name} in #{@actor.shows.sample.title}, I don't know who it is", "#{@actor.name} is jaw-dropping", "Get ready for the return of #{@actor.name} to the A-list", "#{@actor.shows.sample.title} is the hit show of the summer", "There is no examination of our current moment more vital than #{@actor.shows.sample.title}", "Is #{@actor.name} the hottest thing in Hollywood?", "Watch out, Timothée Chalamet: #{@actor.name} is coming for your crown"]
   end
 
   private
