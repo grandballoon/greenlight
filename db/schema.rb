9f037753478a2e2cbd0f4bc42ce99c899ec6d092
grandballoon <luke.sovern.conley@gmail.com>
2018-11-05 13:14:52 -0500

    migrated to postgres from sql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_08_01_200922) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "actors", force: :cascade do |t|
     t.string "name"
     t.boolean "a_list"
